9227f25aed8c8bc45e561bcf883a22269e12b939
92ada <imxintong@gmail.com>
2019-09-19 16:45:44 +0800

    [fixing]

--- src/main/java/com/techncat/quantum/app/controller/UserController.java
+++ src/main/java/com/techncat/quantum/app/controller/UserController.java
@@ -10,7 +10,7 @@ import org.springframework.web.bind.annotation.*;
 import java.util.List;
 
 @RestController
-@RequestMapping("/api/people")
+@RequestMapping("/x/api/people")
 public class UserController {
 
     @Autowired
