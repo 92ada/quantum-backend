130a5ca8abbd1bc0812942f40c655058fd5cb6e6
92ada <imxintong@gmail.com>
2019-11-12 17:23:11 +0800

    [fix]vos

--- src/main/java/com/techncat/quantum/app/vos/daily/TravelVO.java
+++ src/main/java/com/techncat/quantum/app/vos/daily/TravelVO.java
@@ -2,10 +2,12 @@ package com.techncat.quantum.app.vos.daily;
 
 import com.techncat.quantum.app.common.voenhance.annotation.ValueType;
 import com.techncat.quantum.app.model.daily.Travel;
+import lombok.Data;
 
 import java.math.BigDecimal;
 import java.util.Date;
 
+@Data
 public class TravelVO {
     private Long id;
 
@@ -23,84 +25,4 @@ public class TravelVO {
     @ValueType("enumerated")
     private Travel.identity_type identity_type;
     private String identity_no;
-
-    public Long getId() {
-        return id;
-    }
-
-    public void setId(Long id) {
-        this.id = id;
-    }
-
-    public Date getUpdateAt() {
-        return updateAt;
-    }
-
-    public void setUpdateAt(Date updateAt) {
-        this.updateAt = updateAt;
-    }
-
-    public Date getCreatedAt() {
-        return createdAt;
-    }
-
-    public void setCreatedAt(Date createdAt) {
-        this.createdAt = createdAt;
-    }
-
-    public String getTravelerJson() {
-        return travelerJson;
-    }
-
-    public void setTravelerJson(String travelerJson) {
-        this.travelerJson = travelerJson;
-    }
-
-    public Travel.Type getType() {
-        return type;
-    }
-
-    public void setType(Travel.Type type) {
-        this.type = type;
-    }
-
-    public BigDecimal getBudget() {
-        return budget;
-    }
-
-    public void setBudget(BigDecimal budget) {
-        this.budget = budget;
-    }
-
-    public Date getStart_date() {
-        return start_date;
-    }
-
-    public void setStart_date(Date start_date) {
-        this.start_date = start_date;
-    }
-
-    public Date getEnd_date() {
-        return end_date;
-    }
-
-    public void setEnd_date(Date end_date) {
-        this.end_date = end_date;
-    }
-
-    public Travel.identity_type getIdentity_type() {
-        return identity_type;
-    }
-
-    public void setIdentity_type(Travel.identity_type identity_type) {
-        this.identity_type = identity_type;
-    }
-
-    public String getIdentity_no() {
-        return identity_no;
-    }
-
-    public void setIdentity_no(String identity_no) {
-        this.identity_no = identity_no;
-    }
 }
--- src/main/java/com/techncat/quantum/app/vos/daily/VisitFlightVO.java
+++ src/main/java/com/techncat/quantum/app/vos/daily/VisitFlightVO.java
@@ -1,7 +1,10 @@
 package com.techncat.quantum.app.vos.daily;
 
+import lombok.Data;
+
 import java.util.Date;
 
+@Data
 public class VisitFlightVO {
     private Long id;
 
@@ -14,76 +17,4 @@ public class VisitFlightVO {
     private String arrival_site;
     private String flight_no;
     private String price;
-
-    public Long getId() {
-        return id;
-    }
-
-    public void setId(Long id) {
-        this.id = id;
-    }
-
-    public Date getUpdateAt() {
-        return updateAt;
-    }
-
-    public void setUpdateAt(Date updateAt) {
-        this.updateAt = updateAt;
-    }
-
-    public Date getCreatedAt() {
-        return createdAt;
-    }
-
-    public void setCreatedAt(Date createdAt) {
-        this.createdAt = createdAt;
-    }
-
-    public Date getDeparture_time() {
-        return departure_time;
-    }
-
-    public void setDeparture_time(Date departure_time) {
-        this.departure_time = departure_time;
-    }
-
-    public Date getArrival_time() {
-        return arrival_time;
-    }
-
-    public void setArrival_time(Date arrival_time) {
-        this.arrival_time = arrival_time;
-    }
-
-    public String getDeparture_site() {
-        return departure_site;
-    }
-
-    public void setDeparture_site(String departure_site) {
-        this.departure_site = departure_site;
-    }
-
-    public String getArrival_site() {
-        return arrival_site;
-    }
-
-    public void setArrival_site(String arrival_site) {
-        this.arrival_site = arrival_site;
-    }
-
-    public String getFlight_no() {
-        return flight_no;
-    }
-
-    public void setFlight_no(String flight_no) {
-        this.flight_no = flight_no;
-    }
-
-    public String getPrice() {
-        return price;
-    }
-
-    public void setPrice(String price) {
-        this.price = price;
-    }
 }
