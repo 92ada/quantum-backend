326edd9e1bdd869c9f18606b0b40b42a0b89ed2f
cocode <seeucoco@163.com>
2019-11-18 03:28:31 +0800

    export & import (excel module)

--- src/main/java/com/techncat/quantum/app/excel/downloader/PeopleExcelController.java
+++ src/main/java/com/techncat/quantum/app/excel/downloader/PeopleExcelController.java
@@ -17,6 +17,7 @@ import org.springframework.web.multipart.MultipartFile;
 import javax.annotation.Resource;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
+import java.util.ArrayList;
 import java.util.List;
 import java.util.stream.Collectors;
 
@@ -31,15 +32,42 @@ import java.util.stream.Collectors;
 public class PeopleExcelController {
     @Autowired
     private People_SearchService people_searchService;
+    @Resource
+    private People_Repository people_repository;
     @Autowired
     private ExcelService excelService;
 
+    /**
+     * 模版下载
+     *
+     * @param response
+     * @throws IOException
+     */
+    @GetMapping("/{anyname}-template.xls")
+    public void aexcelModel(HttpServletResponse response) throws IOException {
+        List<PeopleRow> peopleRows = new ArrayList<>();
+        PeopleRow row = new PeopleRow();
+        peopleRows.add(row);
+        excelService.export(peopleRows, response.getOutputStream());
+
+    }
+
+    /**
+     * 导出
+     *
+     * @param word
+     * @param type
+     * @param order
+     * @param byProp
+     * @param response
+     * @throws IOException
+     */
     @GetMapping("/{anyname}.xls") // 导出后下载保存名字为：anyname.xls
-    public void export(@RequestParam(value = "word", required = false) String word,
-                       @RequestParam(value = "type", required = false) People.Type type,
-                       @RequestParam(value = "order", defaultValue = "desc") String order,
-                       @RequestParam(value = "by", defaultValue = "createdAt") String byProp,
-                       HttpServletResponse response) throws IOException {
+    public void excelExport(@RequestParam(value = "word", required = false) String word,
+                            @RequestParam(value = "type", required = false) People.Type type,
+                            @RequestParam(value = "order", defaultValue = "desc") String order,
+                            @RequestParam(value = "by", defaultValue = "createdAt") String byProp,
+                            HttpServletResponse response) throws IOException {
         Sort sort = null;
         if (order.toLowerCase().equals("desc")) {
             sort = Sort.by(byProp).descending();
@@ -56,9 +84,13 @@ public class PeopleExcelController {
         excelService.export(peoplePage.getContent().parallelStream().map(PeopleRow::render).collect(Collectors.toList()), response.getOutputStream());
     }
 
-    @Resource
-    private People_Repository people_repository;
-
+    /**
+     * 导入
+     *
+     * @param file
+     * @return
+     * @throws IOException
+     */
     @PostMapping
     public ResponseEntity excelImport(MultipartFile file) throws IOException {
         List<People> data = excelService.read(file, PeopleRow.class).parallelStream().map(PeopleRow::load).collect(Collectors.toList());
