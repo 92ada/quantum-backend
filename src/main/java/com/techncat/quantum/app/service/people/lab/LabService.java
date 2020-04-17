289ec3c0b23501ba466ace5e18de58faa408fbc2
92ada <imxintong@gmail.com>
2020-04-17 10:00:26 +0800

    [fix]people $ref

--- src/main/java/com/techncat/quantum/app/service/people/lab/LabService.java
+++ src/main/java/com/techncat/quantum/app/service/people/lab/LabService.java
@@ -3,6 +3,7 @@ package com.techncat.quantum.app.service.people.lab;
 import com.techncat.quantum.app.common.auth.AuthUtil;
 import com.techncat.quantum.app.common.voutils.VOUtils;
 import com.techncat.quantum.app.model.people.Lab;
+import com.techncat.quantum.app.model.people.People;
 import com.techncat.quantum.app.repository.people.LabRepository;
 import com.techncat.quantum.app.vos.people.LabVO;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -46,9 +47,13 @@ public class LabService {
     }
 
     private static Lab avoidRef(Lab lab) {
+        People _pi = new People();
+        _pi.setId(lab.getPi().getId());
+        _pi.setName(lab.getPi().getName());
+
         Lab labCopy = new Lab();
         labCopy.setId(lab.getId());
-        labCopy.setPi(lab.getPi());
+        labCopy.setPi(_pi);
         labCopy.setDescription(lab.getDescription());
         return labCopy;
     }
@@ -56,6 +61,10 @@ public class LabService {
     public Lab fetch(Long id) {
         Lab found = repository.findFirstById(id);
         if (found == null) throw new LabNotFoundException(id);
+        // avoid $ref of found.fatherLab
+        if (found.getFatherLab() != null) {
+            found.setFatherLab(avoidRef(found.getFatherLab()));
+        }
         return found;
     }
 
